896414bcecf679ff4bda389f1113a4fe06d75826
aung <aungnyeinchann416@gmail.com>
2025-05-30 16:52:50 +0630

    finish cartLists

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -19,19 +19,46 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  // find the cartitem to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  // check if quantity is equal to one , if it is remove that item from cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // return back cartitems with matching cart item with reduced quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+const clearCartItem = (cartItems, cartItemToClear) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
+  // for cartCount
   useEffect(() => {
     const newCartCount = cartItems.reduce(
       (total, cartItem) => total + cartItem.quantity,
@@ -40,16 +67,39 @@ export const CartProvider = ({ children }) => {
     setCartCount(newCartCount);
   }, [cartItems]);
 
+  // for cartTotal
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+    setCartTotal(newCartTotal);
+  }, [cartItems]);
+
+  // adding to the cart and increment
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  // removing from the cart and decrement
+  const removeItemFromCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
+  // removing all items from the cart
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
   };
 
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
+    removeItemFromCart,
+    clearItemFromCart,
     cartItems,
     cartCount,
+    cartTotal,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,14 +1,33 @@
-import { createContext, useState } from "react";
+// import { createContext, useState } from 'react';
 
+// import PRODUCTS from '../shop-data.json';
+
+// export const ProductsContext = createContext({
+//   products: [],
+// });
+
+// export const ProductsProvider = ({ children }) => {
+//   const [products, setProducts] = useState(PRODUCTS); //==> when setProducts is need to use , i reuse this
+//   const value = { products };
+//   return (
+//     <ProductsContext.Provider value={value}>
+//       {children}
+//     </ProductsContext.Provider>
+//   );
+// };
+
+import { createContext, useState } from "react";
 import PRODUCTS from "../shop-data.json";
 
-export const ProductsContext = createContext({});
+export const ProductsContext = createContext({
+  products: [],
+});
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
-  const value = { products };
+  const [products] = useState(PRODUCTS);
+
   return (
-    <ProductsContext.Provider value={value}>
+    <ProductsContext.Provider value={{ products }}>
       {children}
     </ProductsContext.Provider>
   );
